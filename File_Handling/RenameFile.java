5a6f34016ab73043ca139e778422a34821448a87
Pratyush2004-oss <pratyush@sitp.ac.in>
2024-05-14 07:50:33 +0530

    Adding Java Program

--- File_Handling/RenameFile.java
+++ File_Handling/RenameFile.java
@@ -8,16 +8,16 @@ public class RenameFile {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter the current file name with extension: ");
         String currFileName = scan.nextLine();
-        File f = new File("File_Handling//" + currFileName);
-        if (!f.exists()) {
+        File currFile = new File("File_Handling//" + currFileName);
+        if (!currFile.exists()) {
             System.out.println("The file does not exist.");
         }
         else{
-            System.out.println("Current file name is " + f.getName());
+            System.out.println("Current file name is " + currFile.getName());
             System.out.print("Now enter the new file name with extension:");
             String newFileName = scan.nextLine();
             File newFile = new File("File_Handling//"+ newFileName);
-            f.renameTo(newFile);
+            currFile.renameTo(newFile);
             System.out.println("File Succesfully Renamed");
         }
     }
