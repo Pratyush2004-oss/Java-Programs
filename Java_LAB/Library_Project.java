12c7e507ac34f3a20263051b27d943339903b4e5
Pratyush2004-oss <pratyush@sitp.ac.in>
2024-05-16 07:11:57 +0530

    Adding Java Programs

--- Java_LAB/Library_Project.java
+++ Java_LAB/Library_Project.java
@@ -3,47 +3,113 @@ package Java_LAB;
 import java.util.Scanner;
 
 class Book {
-    String[] BookCollection = {"Python","Java","C++","Web Dev","Artificial Intelligance", "Machine Learning", "Data Science"};
+    String[] BookCollection = { "Python", "Java", "C++", "Web Dev", "Artificial Intelligance", "Machine Learning",
+            "Data Science" };
     @SuppressWarnings("resource")
-    void Search(){
+    Scanner sc = new Scanner(System.in);
+    public String item = null;
+
+    String Input() {
+        System.out.print("\nEnter the book you are searching for : ");
+        item = sc.nextLine();
+        return item;
+    }
+
+    String Search() {
         System.out.println("****************  Searching for a book  ********************");
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the Book you are searching for : ");
-        String item = sc.nextLine();
         int count = 0;
-        for(String i : BookCollection){
-            if (item.equalsIgnoreCase(i)){
+        item = Input();
+        for (String i : BookCollection) {
+            if (item.equalsIgnoreCase(i)) {
                 System.out.println(i + "..... Book Available!.........");
                 count = 1;
+                item = i;
+                return item;
             }
         }
-        if(count == 0){
-            System.out.println("Sorry, No such book found!");   
+        if (count == 0) {
+            System.out.println("Sorry, No such book found!");
+        }
+        return null;
+    }
+
+    void Issue() {
+        System.out.println("\n*************   Issuing a book   *******************\n");
+        int count = 0;
+        item = Input();
+        for (String i : BookCollection) {
+            if (item.equalsIgnoreCase(i)) {
+                count = 1;
+                System.out.print("Enter the days for which you want to issue the book : ");
+                int days = sc.nextInt();
+                System.out.println("The book " + item + " will be issued for " + days + " Days.");
+            }
+        }
+        if (count == 0) {
+            System.out.println("Sorry, No such book found!");
         }
     }
 
-    void Issue(){
-        System.out.println("\n*************   Issuing a book   *******************\n");
-    }
-
-    void Renew(){
+    void Renew() {
         System.out.println("\n*************   Renewing a book   ******************\n");
+        item = Input();
+        int count = 0;
+        for (String i : BookCollection) {
+            if (item.equalsIgnoreCase(i)) {
+                count = 1;
+                System.out.print("Enter the days for which you want to Renew the issuing days for the book : ");
+                int days = sc.nextInt();
+                System.out.println("The issue date of The book " + item + " will be extended for " + days + " Days.");
+            }
+        }
+        if (count == 0){
+            System.out.println("This Book is not of our Library......");
+        }
     }
 
-    void Return(){
+    void Return() {
         System.out.println("\n*************   Returning a book   ******************\n");
     }
 
-    void FineCollection(){
+    void FineCollection() {
         System.out.println("\n*************   Showing Fine Collection   ******************\n");
     }
 
 }
 
 public class Library_Project {
+    @SuppressWarnings("resource")
     public static void main(String[] args) {
-        Book b = new Book();
-        b.Search();
+        Scanner sc = new Scanner(System.in);
+        int choice;
+        do {
+            System.out.println(" Press 1 for Search...\n Press 2 for Issuing...\n Press 3 for Renew\n Press 4 for Return... \n Press 5 for Fine Calculation\n\n");
+            System.out.print("Enter Your Choice : ");
+            choice = sc.nextInt();
+            switch (choice) {
+                case 1: {
+                    new Book().Search();
+                }
+                    break;
+                case 2: {
+                    new Book().Issue();
+                }
+                    break;
+                case 3: {
+                    new Book().Renew();
+                    break;
+                }
+                case 4: {
+
+                }
+                case 5: {
+
+                }
+                default: {
+                    System.out.println("Invalid Choice");
+                }
+            }
+        } while (choice != 5);
     }
 
 }
