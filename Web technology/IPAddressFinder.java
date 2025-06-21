951996a689adf3d37de195e241cfe64aa72f06e1
Pratyush2004-oss <pratyush@sitp.ac.in>
2025-06-21 22:34:34 +0530

    changes

--- Web technology/IPAddressFinder.java
+++ Web technology/IPAddressFinder.java
@@ -7,7 +7,6 @@ public class IPAddressFinder {
             // Get the localhost
             InetAddress localhost = InetAddress.getLocalHost();
             System.out.println("Local host IP Address : " + localhost.getHostAddress());
-
             // Get all Ip Address associated with the localhost
             InetAddress[] allLocalAddresses = InetAddress.getAllByName(localhost.getCanonicalHostName());
             System.out.println("All IP Addresses associated with Localhost : \n");
